d08f5806142c0810a833463742c86bcc25352ef3
Alessandro Caetano <alessandro.caetanob@gmail.com>
2016-12-10 13:50:29 -0200

    Last commit

--- embarcado.py
+++ embarcado.py
@@ -6,9 +6,12 @@ usage ./embarcado
 import time
 import math
 import json
-import smbus
+#import smbus
 import serial
-from concurrent.futures import ProcessPoolExecutor
+import numpy as np
+from scipy.linalg import solve
+from threading import Thread
+import random
 import requests
 from   serial.tools import list_ports
 
@@ -21,10 +24,14 @@ PCF8591_ADDRESS = 0x70
 '''
 Global variables for engine and angle normalization
 '''
-engine_vel = {}
-angle = {}
-calibration = True
-d1 = d2 = d3 = velocity = angle = 0.0
+drag = lift = d1 = d2 = d3 = velocity = engine_vel = angle = 0.0
+
+serialCon = bus = None
+
+'''
+Set to true to run simmulation
+'''
+test = True
 
 '''
 Find and open a serial connection with Arduino.
@@ -35,33 +42,38 @@ None : Wait forever, block call
 0    : Non-blocking mode, return immediately
 X    : Any number, float included, timeout block call
 '''
-portName           = list(list_ports.grep(ARDUINO_VID_PID))[0][0]
-serialCon          = serial.Serial()
-serialCon.port     = portName
-serialCon.baudrate = 9600
-serialCon.bytesize = serial.EIGHTBITS
-serialCon.parity   = serial.PARITY_NONE
-serialCon.stopbits = serial.STOPBITS_ONE
-serialCon.timeout  = 1
-serialCon.xonxoff  = False #disable software flow control
-serialCon.rtscts   = False #disable hardware (RTS/CTS) flow control
-serialCon.dsrdtr   = False #disable hardware (DSR/DTR) flow control
-try:
-    serialCon.open()
-except Exception as e:
-    print('Error opening serial port: ' + str(e))
-
-if serialCon.isOpen():
+def openSerialCon():
+    global serialCon
+    portName           = list(list_ports.grep(ARDUINO_VID_PID))[0][0]
+    serialCon          = serial.Serial()
+    serialCon.port     = portName
+    serialCon.baudrate = 9600
+    serialCon.bytesize = serial.EIGHTBITS
+    serialCon.parity   = serial.PARITY_NONE
+    serialCon.stopbits = serial.STOPBITS_ONE
+    serialCon.timeout  = 1
+    serialCon.xonxoff  = False #disable software flow control
+    serialCon.rtscts   = False #disable hardware (RTS/CTS) flow control
+    serialCon.dsrdtr   = False #disable hardware (DSR/DTR) flow control
     try:
-        serialCon.flushInput() #Flush input buffer
-        serialCon.flushOutput() #Flush output buffer
+        serialCon.open()
     except Exception as e:
-        print('Error flushing serial port: ' + str(e))
+        print('Error opening serial port: ' + str(e))
+
+    if serialCon.isOpen():
+        try:
+            serialCon.flushInput() #Flush input buffer
+            serialCon.flushOutput() #Flush output buffer
+        except Exception as e:
+            print('Error flushing serial port: ' + str(e))
 
 #Open i2c connection
-bus = smbus.SMBus(1)
+def openI2C():
+    global bus
+    bus = smbus.SMBus(1)
 
 def calcAngle(x,y,z):
+    global angle
     x = float(x)
     y = float(y)
     z = float(z)
@@ -72,16 +84,24 @@ def calcAngle(x,y,z):
     print('X: '+ str(angleX))
     print('Y: ' + str(angleY))
     print('Z: ' + str(angleZ))
-
+    angle = angleY
 
 '''
 Write to i2c raspberry pin port
 '''
 def busWrite():
-    global engine_vel, velocity
-    value = engine_vel.get(velocity)
+    global bus, velocity 
+    value = velocity
     bus.write_byte_data(PCF8591_ADDRESS, 0x44, value)
 
+'''
+Calculates lift and drag for parameters
+'''
+def calculateLiftDrag(d1,d2,d3):
+    global lift, drag, angle
+    a = np.array([[math.sin(angle), math.cos(angle)],[(59.5*math.sin(angle) + 357.5*math.cos(angle)), 357.5*math.sin(angle)]])
+    b =  np.array([d2, (102.91*d1 + 45.3*d3)])
+    lift, drag = solve(a, b)
 '''
 Writes data on device on serial port. Called by assyncronous task to read from server
 '''
@@ -100,14 +120,19 @@ Writes data on device on serial port. Called by assyncronous task to read from s
 Assyncronous task to read data from serial port
 '''
 def serialReader():
-    global serialCon, d1, d2, d3, angle
+    global serialCon, d1, d2, d3, angle, engine_vel
     while True:
         if serialCon.isOpen():
             try:
                 inc_bytes = serialCon.inWaiting()
+                data = inc_bytes.split()
+                angle = calcAngle(data[1],data[3],data[5])
+                d1, d2, d3 = data[7], data[9], data[11]
+                lift, drag = calculate_lift_drag(d1,d2,d3)
+                engine_vel = data[13]
             except Exception as e:
                 print('Could not read data from serial con: ' + str(e))
-        time.sleep(1)
+        time.sleep(0.5)
 
 '''
 Method to close serial connection when needed
@@ -127,59 +152,81 @@ Assyncronous task to read from web server
 def readWebServer():
     global velocity
     while True:
-        data = requests.get('', verify=False).json()
-        time.sleep(1)
+        data = requests.get('https://ancient-beach-55720.herokuapp.com/tests/1', verify=False).json()
+        time.sleep(0.5)
+        velocity = data['intended_velocity']
+        print('Read velocity from APP is: ' + str(velocity))
 
 
 '''
 Writing to web server via post api, currently only printing on terminal
 '''
-def writeToWebServer(values):
-    global velocity, angle, d1, d2, d3
-    data = {"test":{"velocity": values[0], "angle": values[1]}}
-    json_data = json.dumps(data)
-    headers = {'Content-type': 'application/json'}
-    request = requests.put('http://192.168.15.11:3000/tests/1', data=json_data, headers=headers)
-    print(request.status_code, request.reason)
-    print(data)
+def writeToWebServer():
+    global engine_vel, angle, lift, drag
+    while True:
+        data = {"test":{"velocity": engine_vel, "angle": angle, "drag": drag, "lift": lift}}
+        json_data = json.dumps(data)
+        headers = {'Content-type': 'application/json'}
+        request = requests.put('https://ancient-beach-55720.herokuapp.com/tests/1', data=json_data, headers=headers)
+        print(request.status_code, request.reason)
+        print(data)
 
-#def calibration():
-#    global calibration
-#    global engine_vel
-#    if callibrarion == False:
-#            break
-#    elif calibration == True:
-#        for i in range(0,255):
+'''
+Run simulation of parameters
+'''
+def runSimulation():
+    global drag, lift, d1, d2, d3, velocity, engine_vel, angle
+    t1 = Thread(target = readWebServer)
+    t2 = Thread(target = writeToWebServer)
 
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    
+    t1.start()
+    t2.start()
+    while True:
+        with open('sample_data', 'r+') as infile: 
+            for line in infile:
+                line = line.split()
+                drag = line[1] 
+                lift = line[3]
+                d1 = line[5]
+                d2 = line[7]
+                d3 = line[9]
+                velocity = line[11]
+                engine_vel = line[13] 
+                angle = line[15]
+              
 
 '''
 main funtcion
 '''
 def main():
-    pool = ProcessPoolExecutor(4);
-    try:
-        serial_reader_future = pool.submit(serialReader)
-        bus_writer_future = pool.submit(busWrite)
-        web_writer_future = pool.submit(writeToWebServer)
-        web_reader_future = pool.submit(readWebServer)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        serial_reader_future.done()
-        bus_writer_future.done()
-        web_writer_future.done()
-        web_reader_future.done()
-
-    #loop = asyncio.get_event_loop()
-    #try:
-    #    asyncio.ensure_future(serialReader())
-    #    asyncio.ensure_future(readWebServer())
-    #    loop.run_forever()
-    #except KeyboardInterrupt:
-    #    pass
-    #finally:
-    #    loop.close()
-    #    closeSerialConnection()
+    if test is False:
+        try:
+            openSerialCon()
+            openI2C()
+            t1 = Thread(target = serialReader)
+            t2 = Thread(target = readWebServer)
+            t3 = Thread(target = writeToWebServer)
+            t4 = Thread(target = busWrite)
+            
+            t1.setDaemon(True)
+            t2.setDaemon(True)
+            t3.setDaemon(True)
+            t4.setDaemon(True)
+        
+            t1.start()
+            t2.start()
+            t3.start()
+            t4.start()
+        
+        except KeyboardInterrupt:
+            pass
+        finally:
+            closeSerialConnection()
+    else:
+        runSimulation()
 
 if __name__  == '__main__':
     main()
